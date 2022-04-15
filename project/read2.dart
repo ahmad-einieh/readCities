31c3ac835992094884d71454a347838dd4944d7d
ahmad <abomoaz3375@gmail.com>
2022-04-15 19:13:52 +0300

    a

--- project/read2.dart
+++ project/read2.dart
@@ -5,13 +5,13 @@ import 'dart:io';
 main() {
   List<String> x;
   String x1, x2, x3;
-  new File("test.txt")
+  new File("cities2.txt")
       .openRead()
       .transform(utf8.decoder)
       .transform(new LineSplitter())
       .forEach((l) {
-    x = l.split(",");
-    print("City(${x.elementAt(0)} , ${x.elementAt(1)} , \"${x.elementAt(2)}\") , ");
+    x = l.split(" ");
+    print("Gene(${x.elementAt(1)} , ${x.elementAt(2)} , \"${x.elementAt(0)}\") , ");
 
     /* for(int i=0;i<x.length;i++){
           x1 = x[i];
